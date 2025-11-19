1d564ca15addec3b5b1846b987acc9f4cb357f92
nbogie <neillbogie@googlemail.com>
2025-11-19 16:02:43 +0000

    add breaking p5 code to sketch - won't type-check or run as intended, yet

--- src/main.ts
+++ src/main.ts
@@ -1,1 +1,11 @@
-console.log("hello world");
+console.log("hello world. in main.ts");
+
+function setup() {
+    console.log("in setup()");
+    createCanvas(windowWidth, windowHeight);
+}
+
+function draw() {
+    fill("dodgerblue");
+    circle(width / 2, height / 2, 200);
+}
