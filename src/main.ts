6e66d2c80fd29eb405b58ff71d7fb2fedd9666a3
nbogie <neillbogie@googlemail.com>
2025-11-19 16:13:08 +0000

    register the setup draw fns on window - still won't work

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,15 @@
 console.log("hello world. in main.ts");
 
-function setup() {
+window.setup = function setup() {
     console.log("in setup()");
     createCanvas(windowWidth, windowHeight);
-}
+};
 
-function draw() {
+window.draw = function draw() {
     fill("dodgerblue");
     circle(width / 2, height / 2, 200);
-}
+};
+
+window.mousePressed = function (evt) {
+    text(JSON.stringify(evt, null, 2), 0, 0);
+};
