f4ea7d91634569ce478b504da90d90923939be3c
nbogie <neillbogie@googlemail.com>
2025-11-23 02:05:52 +0000

    experimenting with separate output module files for readability

--- vite-for-no-bundling.config.js
+++ vite-for-no-bundling.config.js
@@ -1,3 +1,7 @@
+//reading:
+//configuring vite: https://vite.dev/config/
+//build options: https://vite.dev/config/build-options
+//building for production: https://vite.dev/guide/build
 import { defineConfig } from "vite";
 
 export default defineConfig({
@@ -5,10 +9,20 @@ export default defineConfig({
         //more readable code for upload on openprocessing etc
         minify: false,
 
+        //maybe use dynamic import vars ?
+        // https://github.com/rollup/plugins/tree/master/packages/dynamic-import-vars
+
         rollupOptions: {
             //externalize deps that shouldn't be bundled
             external: ["dat.gui", "gsap", "p5"],
-            output: {},
+            //https://rollupjs.org/configuration-options/
+            preserveEntrySignatures: "strict",
+            output: {
+                format: "es",
+                preserveModules: true,
+                // chunkFileNames: `[name].js`, // ensures chunks are named clearly
+                entryFileNames: `[name].js`, // ensures module files are named clearly
+            },
         },
     },
 });
