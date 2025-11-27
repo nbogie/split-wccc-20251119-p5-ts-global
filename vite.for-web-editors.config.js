1f52acddb94e17f93155eff63418c6724bacbe83
nbogie <neillbogie@googlemail.com>
2025-11-27 04:55:06 +0000

    disable modulePreload polyfill, add manifest, change to dist-for-web-editors

--- vite.for-web-editors.config.js
+++ vite.for-web-editors.config.js
@@ -5,25 +5,23 @@
 //building for production: https://vite.dev/guide/build
 import { defineConfig } from "vite";
 import { resolve } from "path"; // Need this to resolve the file path
-export default defineConfig(({ mode }) => {
-    // Vite automatically sets mode to 'development' for dev and 'production' for build.
-    const isProduction = mode === "production";
-
+export default defineConfig((_options) => {
     return {
         build: {
+            manifest: "myManifest.json",
             //more readable code for upload on openprocessing etc
             minify: false,
             //maybe use dynamic import vars ?
             // https://github.com/rollup/plugins/tree/master/packages/dynamic-import-vars
 
+            //https://vite.dev/config/build-options#build-modulepreload
+            //remove unreadable polyfill chunk at start of our output
+            //this is the readable version of the build!
+            modulePreload: { polyfill: false },
+
             rollupOptions: {
-                // 2. CONDITIONALLY set the build INPUT file
                 input: {
-                    // If building, use the production HTML file.
-                    // Otherwise (for dev server), Vite defaults to the root index.html
-                    app: isProduction
-                        ? resolve(__dirname, "index.for-web-editors.html")
-                        : resolve(__dirname, "index.html"),
+                    app: resolve(__dirname, "index.for-web-editors.html"),
                 },
 
                 //externalize deps that shouldn't be bundled
@@ -32,6 +30,7 @@ export default defineConfig(({ mode }) => {
                 preserveEntrySignatures: "strict",
                 output: {
                     format: "es",
+                    dir: resolve(__dirname, "dist-for-web-editors"),
                     // preserveModules: true,
                     // chunkFileNames: `[name].js`, // ensures chunks are named clearly
                     // entryFileNames: `[name].js`, // ensures module files are named clearly
